014e84c0bd8abee29e1fb52b9282dc624f9bb020
Rodrigo França <rodrigo.fla75@gmail.com>
2024-06-11 11:22:48 -0300

    upload script sh device tree onclite

---
+++ device-tree-onclite.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/onclite/android_device_xiaomi_onclite device/xiaomi/onclite
+
+git clone https://github.com/onclite/android_kernel_xiaomi_onclite kernel/xiaomi/onclite
+
+git clone https://github.com/onclite/android_vendor_xiaomi_onclite vendor/xiaomi/onclite
+
+git clone https://github.com/onclite/android_kernel_xiaomi_onclite-caf kernel/xiaomi/onclite
+
+git clone https://github.com/onclite/android_vendor_xiaomi_onclite-caf vendor/xiaomi/onclite
+
+git clone https://github.com/onclite/android_device_xiaomi_onclite-caf device/xiaomi/onclite
+
+git clone https://github.com/onclite/android_device_qcom_msm8953-64 device/qcom/msm8953-64
