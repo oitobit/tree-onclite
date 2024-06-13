13aadfd6b8b5bcbbcfb675f57288446478397877
Rodrigo França <rodrigo.fla75@gmail.com>
2024-06-13 10:59:32 -0300

    Change: device tree onclite clone repository update

--- device-tree-onclite.sh
+++ device-tree-onclite.sh
@@ -4,10 +4,4 @@ git clone https://github.com/onclite/android_kernel_xiaomi_onclite kernel/xiaomi
 
 git clone https://github.com/onclite/android_vendor_xiaomi_onclite vendor/xiaomi/onclite
 
-git clone https://github.com/onclite/android_kernel_xiaomi_onclite-caf kernel/xiaomi/onclite
-
-git clone https://github.com/onclite/android_vendor_xiaomi_onclite-caf vendor/xiaomi/onclite
-
-git clone https://github.com/onclite/android_device_xiaomi_onclite-caf device/xiaomi/onclite
-
-git clone https://github.com/onclite/android_device_qcom_msm8953-64 device/qcom/msm8953-64
+git clone https://github.com/hadadarjt/proton-clang -b 20210123 prebuilts/clang/kernel/linux-x86/clang-proton
